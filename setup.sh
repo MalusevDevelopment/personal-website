2bcf77e523b739af5c0229055dbf7c58e9dd8cb5
Dusan Malusev <dusan@dusanmalusev.dev>
2023-08-28 04:06:28 +0200

    Fixing issue with permissions -> not success
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
-make build-base-image
-
-# sudo rm -rf vendor
+#!/usr/bin/env bash
 
 docker run --rm \
     -u "$(id -u):$(id -g)" \
     -v "$(pwd):/var/www/html" \
     -w /var/www/html \
+    -e COMPOSER_ALLOW_SUPERUSER=1 \
     laravelsail/php82-composer:latest \
-    composer install --ignore-platform-reqs
+    composer update --ignore-platform-reqs
 
-./vendor/bin/sail up -d
+cp .env.example .env
+
+./vendor/bin/sail up -d || exit 1
 
 sleep 10
 
-./vendor/bin/sail artisan migrate
-./vendor/bin/sail artisan key:generate
+./vendor/bin/sail artisan migrate || exit 1
+./vendor/bin/sail artisan key:generate || exit 1
 ./vendor/bin/sail artisan storage:link
-./vendor/bin/sail artisan db:seed
+./vendor/bin/sail artisan db:seed || exit 1
