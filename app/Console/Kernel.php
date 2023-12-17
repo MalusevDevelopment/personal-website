0d95b8f909562c3b7497aded97884569a00a6a79
Dusan Malusev <dusan@dusanmalusev.dev>
2023-12-17 13:53:23 +0100

    PHP 8.3 and Debian instead of Ubuntu for Docker
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
+     * @throws BindingResolutionException
      */
     protected function schedule(Schedule $schedule): void
     {
@@ -20,10 +22,11 @@ class Kernel extends ConsoleKernel
 
     /**
      * Register the commands for the application.
+     * @throws BindingResolutionException
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
