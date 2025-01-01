151a55a13b9fa23f340ca8520847628ace221752
Dusan Malusev <dusan.malusev@scylladb.dev>
2025-01-01 18:32:35 +0100

    feature(deploy): deploy project with github actions, and just copy whats needed
    Signed-off-by: Dusan Malusev <dusan.malusev@scylladb.dev>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+WORKDIR=/opt/projects/website
+
+pushd "$WORKDIR" || exit 1
+
+print_usage() {
+    echo "Usage: $0 -u <USERNAME> -r <registry> -i <image> -v <version>"
+    exit 1
+}
+
 run_artisan_command() {
     local service=$1
     shift
@@ -7,12 +16,37 @@ run_artisan_command() {
         # shellcheck disable=SC2068
         docker exec "$container" php artisan $@ -n --ansi
     done
-
 }
+
+IMAGE="ghcr.io/codelieutenant/website"
+VERSION="latest"
+REGISTRY="ghcr.io"
+
+while getopts "r:u:i:v:" option; do
+	case "$option" in
+	r) REGISTRY=$OPTARG ;;
+    u) USERNAME=$OPTARG ;;
+    i) IMAGE=$OPTARG ;;
+    v) VERSION=$OPTARG ;;
+	*) print_usage ;;
+	esac
+done
+
+if [ -z "$USERNAME" ]; then
+  echo "Username is required"
+  exit 1
+fi
+
+docker login "$REGISTRY" --username "$USERNAME" --password-stdin < /dev/stdin || exit 1
+
+docker pull "$IMAGE:$VERSION" || exit 1
+docker logout "$REGISTRY"
 
 run_artisan_command horizon horizon:terminate || exit 1
 
-docker stack deploy --prune --detach --compose-file /opt/website/compose.yml
+VERSION="$VERSION" docker stack deploy --prune --detach --compose-file "$WORKDIR/compose.yml"
 
 run_artisan_command scheduler schedule-monitor:sync
 run_artisan_command scheduler pulse:clear
+
+popd || exit 1
