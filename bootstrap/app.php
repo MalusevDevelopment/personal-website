b5b75c88bcc1ebfbe5a93bf44995329aa5bb8686
Dusan Malusev <dusan.malusev@scylladb.dev>
2025-01-03 20:27:48 +0100

    refactor(boostrap): remove providers.php, reduce IO operations
    Signed-off-by: Dusan Malusev <dusan.malusev@scylladb.dev>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,14 +6,11 @@ use App\Http\Middleware\AddContentSecurityPolicyHeaders;
 use App\Http\Middleware\UserIdMiddleware;
 use App\Models\User;
 use CodeLieutenant\LaravelCrypto\ServiceProvider as LaravelCryptoServiceProvider;
-use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Encryption\EncryptionServiceProvider as LaravelEncryptionServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use Illuminate\Http\Middleware\TrustHosts;
-use Illuminate\Http\Middleware\TrustProxies;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,28 +22,35 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustHosts(at: fn () => config('app.domain'));
+        $middleware->encryptCookies([UserIdMiddleware::COOKIE_NAME]);
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO);
         $middleware->web(append: AddLinkHeadersForPreloadedAssets::class);
         $middleware->web(append: AddContentSecurityPolicyHeaders::class);
         $middleware->web(append: UserIdMiddleware::class);
     })
-    ->withProviders([
-        LaravelEncryptionServiceProvider::class => LaravelCryptoServiceProvider::class,
-    ])
+    ->withProviders(
+        providers: [
+            LaravelEncryptionServiceProvider::class => LaravelCryptoServiceProvider::class,
+            CodeLieutenant\LaravelPgEnum\ServiceProvider::class,
+            App\Providers\PulseServiceProvider::class,
+            App\Providers\AppServiceProvider::class,
+            App\Providers\Filament\AdminPanelProvider::class,
+            App\Providers\HorizonServiceProvider::class,
+            App\Providers\PulseServiceProvider::class,
+            App\Providers\TelescopeServiceProvider::class,
+        ],
+        withBootstrapProviders: false,
+    )
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })
     ->booting(function () {
         Gate::before(static fn (User $user) => $user->isOwner() ? true : null);
-
-        TrustHosts::at([config('app.domain')]);
-        EncryptCookies::except([UserIdMiddleware::COOKIE_NAME]);
-        TrustProxies::at('*');
-        TrustProxies::withHeaders(
-            Request::HEADER_X_FORWARDED_FOR |
-            Request::HEADER_X_FORWARDED_HOST |
-            Request::HEADER_X_FORWARDED_PORT |
-            Request::HEADER_X_FORWARDED_PROTO
-        );
-
     })
     ->create();
